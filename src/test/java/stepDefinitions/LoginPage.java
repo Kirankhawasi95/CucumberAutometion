1cbe88f39bba79d036fd573b4de88aff921992a7
Kiran Prakash Khawasi <kpkmt942@MGTECHSOFT.IN>
2023-07-02 23:04:18 +0530

    initial commit

---
+++ src/test/java/stepDefinitions/LoginPage.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObjects.HomePage;
+import pageObjects.MyAccountPage;
+
+public class LoginPage {
+	
+	 WebDriver driver;
+     HomePage hp;
+     LoginPage lp;
+     MyAccountPage macc;
+
+
+}
